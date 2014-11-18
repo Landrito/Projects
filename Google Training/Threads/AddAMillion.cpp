3b8b1123a1c376fc406d9c351200ceb74c59ae32
Ernest Landrito <landrito@nevada.unr.edu>
2014-11-17 19:18:31 -0800

    halp

--- Google Training/Threads/AddAMillion.cpp
+++ Google Training/Threads/AddAMillion.cpp
@@ -5,20 +5,21 @@ const int NUM_VALUES = 1000000;
 const int NUM_THREADS = 8;
 const int MAX_VALUE = 15 * NUM_VALUES / 1000;
 
+void sumValues(int * arr, int & result, int start, int end);
+void assignValue(int * arr, int start, int end);
 
 int main(){
 	int * array = new int[NUM_VALUES];
 	int * results = new int[NUM_THREADS];
 
 	//ASSIGN THE VALUES
-	std::thread ** threadArray = new std:thread*[NUM_THREADS];
+	std::thread ** threadArray = new std::thread*[NUM_THREADS];
 	for(int i = 0; i < NUM_THREADS; i++){
-		threadArray[i] = new std::thread(sumValues, array, i*(NUM_VALUES / NUM_THREADS), (i+1)*(NUM_VALUES/NUM_THREADS));
+		threadArray[i] = new std::thread(assignValue, array, i*(NUM_VALUES / NUM_THREADS), (i+1)*(NUM_VALUES/NUM_THREADS));
 	}
 
 	for(int i = 0; i < NUM_THREADS; i++){
 		threadArray[i]->join();
-		delete threadArray[i];
 	}
 
 	//CALCULATE THE VALUES
@@ -29,7 +30,6 @@ int main(){
 
 	for(int i = 0; i < NUM_THREADS; i++){
 		threadArray[i]->join();
-		delete threadArray[i];
 	}
 
 	int finalResult = 0;
@@ -42,12 +42,12 @@ int main(){
 
 void sumValues(int * arr, int & result, int start, int end){
 	for(int i = start; i < end; i++){
-		bool prime == true;
+		bool prime = true;
 		for(int j = 2; j <= MAX_VALUE && prime; i++){
 			if(arr[i] == 1){
 				prime = false;
-			} else if(arr[i] % i == 0){
-				prime == false;
+			} else if(arr[i] % j == 0){
+				prime = false;
 			}
 		}
 		if(prime){
@@ -57,7 +57,7 @@ void sumValues(int * arr, int & result, int start, int end){
 }
 
 void assignValue(int * arr, int start, int end){
-	for( int i = start; i < send; i++){
+	for( int i = start; i < end; i++){
 		arr[i] = 15 * i / 1000;
 	}
 }
